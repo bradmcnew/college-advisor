ad8f0d517ec4588b6b825430a5484d4e5d34acce
Brad McNew <mcnewbrad@gmail.com>
2025-05-06 13:15:44 -0400

    add auth to server actions

--- src/app/(default)/(dashboard)/actions.ts
+++ src/app/(default)/(dashboard)/actions.ts
@@ -1,11 +1,13 @@
 "use server";
 
+import { requireAuth } from "~/lib/auth-utils";
 import { getPosts, getPostsByFilters } from "~/server/queries";
 
 /*
  * Infinite scroll server actions
  */
 export const fetchPostsAction = async (limit = 20, offset = 0) => {
+  await requireAuth();
   const posts = await getPosts(limit, offset);
   return posts;
 };
@@ -17,6 +19,7 @@ export const fetchPostsByFilterAction = async (
   limit = 20,
   offset = 0,
 ) => {
+  await requireAuth();
   const posts = await getPostsByFilters(
     schoolId,
     majorId,
